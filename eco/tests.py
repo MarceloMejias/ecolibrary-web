af1c0cddbf7f483a9a53804a776b533d12da721d
Marcelo Mejías <65011000+MarceloMejias@users.noreply.github.com>
2025-11-26 14:08:19 -0300

    se añaden tests para cumplir coverage de sonarqube

--- eco/tests.py
+++ eco/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+class WebViewsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_view(self):
+        """El home debe cargar y usar el template correcto"""
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_login_view_get(self):
+        """El login debe cargar el formulario"""
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+
+    def test_register_view_get(self):
+        """El registro debe cargar el formulario"""
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'register.html')
